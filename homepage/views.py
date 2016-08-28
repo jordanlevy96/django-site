199ad386f2c4484513d473c787c4006f220690e5
jordanlevy96 <jordanlevy96@gmail.com>
2016-08-28 20:46:55 +1000

    removed teapot, wrote basic HTML page

--- homepage/views.py
+++ homepage/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+def post_list(request):
+	return render(request, 'homepage/post_list.html', {})
+
+""" teapot
+
 def index(request):
 	 r = requests.get("http://httpbin.org/status/418")
-	 print r.text
+	 print(r.text)
 	 return HttpResponse("<pre>" + r.text + "</pre>")
+
+"""
--- polls/models.py
+++ polls/models.py
@@ -18,17 +18,3 @@ class Choice(models.Model):
 	 votes = models.IntegerField(default=0)
 	 def __str__(self):
 		  return self.choice_text
-
-class Post(models.Model):
-	author = models.ForeignKey('auth.User')
-	title = models.CharField(max_length=200)
-	text = models.TextField()
-	created_date = models.DateTimeField(default=timezone.now)
-	published_date = models.DateTimeField(blank=True, null=True)
-
-	def publish(self):
-		self.published_date = timezone.now()
-		self.save()
-
-	def __str__(self):
-		return self.title
