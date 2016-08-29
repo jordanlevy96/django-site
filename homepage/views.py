fc89ea81e812d98c8c7b4104d760a3dbe99228d0
jordanlevy96 <jordanlevy96@gmail.com>
2016-08-29 17:17:13 +1000

    added posts to HTML

--- homepage/views.py
+++ homepage/views.py
@@ -1,11 +1,14 @@
 import requests
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse #for the teapot
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-	return render(request, 'homepage/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'homepage/post_list.html', {'posts': posts})
 
 """ teapot
 
