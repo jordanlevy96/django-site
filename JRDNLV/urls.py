ce0685d6f2859da89ef5bd165be512cd27ef4537
jordanlevy96 <jordanlevy96@gmail.com>
2016-08-31 16:27:57 +1000

    made a better url regex

--- JRDNLV/urls.py
+++ JRDNLV/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('homepage.urls')),
+    url(r'^$', include('homepage.urls')),
     url(r'^polls/', include('polls.urls')),
 ]
