6001ddf3645b70de3a318e7798ebacd02dd878b1
jordanlevy96 <jordanlevy96@gmail.com>
2016-08-31 16:05:29 +1000

    reverted -- no more capital Ps

--- homepage/views.py
+++ homepage/views.py
@@ -1,14 +1,12 @@
 import requests
 from django.shortcuts import render
-from django.http import HttpResponse #for the teapot
-from .models import Post
-from django.utils import timezone
+from django.http import HttpResponse
 
 # Create your views here.
 
 def post_list(request):
-	Posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'homepage/post_list.html', {'Posts': Posts})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'homepage/post_list.html', {'posts': posts})
 
 """ teapot
 
