92a0391edaa6d0c211ec95a08b89cde7b8213a01
jordanlevy96 <jordanlevy96@gmail.com>
2016-08-31 15:13:30 +1000

    capitalized some more Ps

--- homepage/views.py
+++ homepage/views.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'homepage/post_list.html', {'posts': posts})
+	Posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'homepage/post_list.html', {'Posts': Posts})
 
 """ teapot
 
