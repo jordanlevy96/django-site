c6534b56bbfdd075fa68b0c6702bc5156d326e71
jordanlevy96 <jordanlevy96@gmail.com>
2016-10-28 22:54:29 +1100

    renamed 'homepage' to 'blog' :

--- blog/views.py
+++ blog/views.py
@@ -9,11 +9,11 @@ from .forms import PostForm
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'homepage/post_list.html', {'posts': posts})
+	return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
-	return render(request, 'homepage/post_detail.html', {'post': post})
+	return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
 	if request.method == "POST":
@@ -26,7 +26,7 @@ def post_new(request):
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm()
-	return render(request, 'homepage/post_edit.html', {'form': form})
+	return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
@@ -40,7 +40,7 @@ def post_edit(request, pk):
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
-	return render(request, 'homepage/post_edit.html', {'form': form})
+	return render(request, 'blog/post_edit.html', {'form': form})
 
 """ teapot
 
