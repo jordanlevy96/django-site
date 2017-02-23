3260a24e8d8c349b38a1695aaf15ac4b38d5c5d5
jordanlevy96 <jordanlevy96@gmail.com>
2017-02-23 15:00:15 -0800

    tried to add 404/500 page...don't think it works right

--- JRDNLV/urls.py
+++ JRDNLV/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-	 url(r'^$', include('homepage.urls')),
+	url(r'^home/', include('homepage.urls')),
     url(r'^blog/', include('blog.urls')),
+    url(r'^$', RedirectView.as_view(url='home', permanent=False), name='home') #redirects from base site to home
+    #url(r'^.*$', RedirectView.as_view(url='404', permanent=False), name='404') #redirects from random shit to 404
 ]
